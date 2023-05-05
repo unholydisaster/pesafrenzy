524257b06f05e953b95ad862f3347013c73502a3
unholydisaster <eli.musa.173@gmail.com>
2023-05-05 22:28:28 +0300

    changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import { GlobalStyle} from "../styles/Globals.js";
+import React from "react"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+export default function App({Component,pageProps}){
+  return (
+    <>
+      <GlobalStyle/>
+      <Component {...pageProps} />  
+    </>
+  )
 }
+
