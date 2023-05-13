aefca71e93d43d04b11da2c5f24fadecd65c3dd6
unholydisaster <eli.musa.173@gmail.com>
2023-05-13 18:30:22 +0300

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const MyForm = () => {
 
     axios.post('/api/lipanampesa', requestData)
       .then(response => {
-        console.log(response.data);
+        console.log(response.data,orderId);
         // handle successful response here
       })
       .catch(error => {
