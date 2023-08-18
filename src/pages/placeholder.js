5439f7c561d41bd914c6044ef6e857d6de037c5e
Eli musa <60873440+unholydisaster@users.noreply.github.com>
2023-08-18 13:11:58 +0300

    Create placeholder.js

---
+++ src/pages/placeholder.js
@@ -0,0 +1,23 @@
+export const store = [
+    { id: "1", model: 'Model 1', name: 'Item 1', yearfrom: 2000, yearto: 2005 },
+    { id: "2", model: 'Model 2', name: 'Item 2', yearfrom: 2005, yearto: 2010 },
+    { id: "3", model: 'Model 3', name: 'Item 3', yearfrom: 2010, yearto: 2015 },
+    { id: "4", model: 'Model 4', name: 'Item 4', yearfrom: 2015, yearto: 2020 },
+    { id: "5", model: 'Model 5', name: 'Item 5', yearfrom: 2020, yearto: 2025 },
+    { id: "6", model: 'Model 6', name: 'Item 6', yearfrom: 2000, yearto: 2005 },
+    { id: "7", model: 'Model 7', name: 'Item 7', yearfrom: 2005, yearto: 2010 },
+    { id: "8", model: 'Model 8', name: 'Item 8', yearfrom: 2010, yearto: 2015 },
+    { id: "9", model: 'Model 9', name: 'Item 9', yearfrom: 2015, yearto: 2020 },
+    { id: "10", model: 'Model 10', name: 'Item 10', yearfrom: 2020, yearto: 2025 },
+    { id: "11", model: 'Model 11', name: 'Item 11', yearfrom: 2000, yearto: 2005 },
+    { id: "12", model: 'Model 12', name: 'Item 12', yearfrom: 2005, yearto: 2010 },
+    { id: "13", model: 'Model 13', name: 'Item 13', yearfrom: 2010, yearto: 2015 },
+    { id: "14", model: 'Model 14', name: 'Item 14', yearfrom: 2015, yearto: 2020 },
+    { id: "15", model: 'Model 15', name: 'Item 15', yearfrom: 2020, yearto: 2025 },
+    { id: "16", model: 'Model 16', name: 'Item 16', yearfrom: 2000, yearto: 2005 },
+    { id: "17", model: 'Model 17', name: 'Item 17', yearfrom: 2005, yearto: 2010 },
+    { id: "18", model: 'Model 18', name: 'Item 18', yearfrom: 2010, yearto: 2015 },
+    { id: "19", model: 'Model 19', name: 'Item 19', yearfrom: 2015, yearto: 2020 },
+    { id: "20", model: 'Model 20', name: 'Item 20', yearfrom: 2020, yearto: 2025 },
+  ];
+  
