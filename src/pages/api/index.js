0ac59600f9e0c103e7d79b2d0922fcefac8a1003
Eli musa <60873440+unholydisaster@users.noreply.github.com>
2023-08-18 13:05:22 +0300

    Add files via upload

---
+++ src/pages/api/index.js
@@ -0,0 +1,27 @@
+import { store } from "../placeholder";
+
+export default async(req, res)=> {
+  let { query:{page,limit},method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        
+        if(!page) page=1
+        if(!limit) limit=10
+        page=parseInt(page)
+        limit=parseInt(limit)
+        return res.json({
+            data:store.slice((page-1)*limit, page * limit),
+            total:store.length
+        })
+        
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+};
